60118e8fe443b2c962416fc39d66cf8f344e640e
Zacharias Kull <zacharias.kull@elca.ch>
2020-06-25 14:15:35 +0200

    update for release 1.0.5

--- src/main/scala/com/sample/ProgrammaticAccessDemo.scala
+++ src/main/scala/com/sample/ProgrammaticAccessDemo.scala
@@ -3,6 +3,7 @@ package com.sample
 import io.smartdatalake.config.ConfigToolbox
 import io.smartdatalake.workflow.dataobject.CsvFileDataObject
 import io.smartdatalake.config.SdlConfigObject.stringToDataObjectId
+import org.apache.spark.sql.SparkSession
 
 object ProgrammaticAccessDemo extends App {
 
@@ -10,7 +11,7 @@ object ProgrammaticAccessDemo extends App {
   val (registry, globalConfig) = ConfigToolbox.loadAndParseConfig(Seq("../src/main/resources"))
 
   // create spark session if not provided by environment
-  implicit val session = globalConfig.createSparkSession("test")
+  implicit val session: SparkSession = globalConfig.createSparkSession("test", Some("local[*]"))
 
   // get data object of the registry
   val dataObject = registry.get[CsvFileDataObject]("ab-csv-org")
