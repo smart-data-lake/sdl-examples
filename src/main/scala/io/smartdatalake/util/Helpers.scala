9805f235165942356b8fd2b50fc9b657e41094ec
Zacharias Kull <zacharias.kull@elca.ch>
2020-12-03 09:18:13 +0100

    fix using log4j2

--- src/main/scala/io/smartdatalake/util/Helpers.scala
+++ src/main/scala/io/smartdatalake/util/Helpers.scala
@@ -5,6 +5,6 @@ import io.smartdatalake.config.InstanceRegistry
 import io.smartdatalake.workflow.{ActionPipelineContext, ExecutionPhase}
 
 object Helpers {
-  // TODO: unfortunately ActionPipelineContext is only accessible within io.smartdatalke. This will be fixed in a future version
+  // TODO: unfortunately ActionPipelineContext is only accessible within io.smartdatalake. This will be fixed in a future version
   def getDefaultExecContext(registry: InstanceRegistry): ActionPipelineContext = ActionPipelineContext("test", "sandbox", 1, 1, registry, appConfig = SmartDataLakeBuilderConfig(), phase = ExecutionPhase.Exec)
 }
