e556c3707505cca54d894f8828ebe3eb9b1ddb6b
Zacharias Kull <zacharias.kull@elca.ch>
2021-08-31 11:56:42 +0200

    adapt for SDL version 2.1.1

--- src/main/scala/com/sample/ProgrammaticAccessDemo.scala
+++ src/main/scala/com/sample/ProgrammaticAccessDemo.scala
@@ -1,10 +1,11 @@
 package com.sample
 
-import io.smartdatalake.config.ConfigToolbox
 import io.smartdatalake.config.SdlConfigObject.stringToDataObjectId
-import io.smartdatalake.util.Helpers
-import io.smartdatalake.workflow.dataobject.CsvFileDataObject
+import io.smartdatalake.config.ConfigToolbox
+import io.smartdatalake.testutils.TestUtil
+import io.smartdatalake.workflow.dataobject.{CsvFileDataObject, ParquetFileDataObject}
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
 
 object ProgrammaticAccessDemo extends App {
 
@@ -13,7 +14,8 @@ object ProgrammaticAccessDemo extends App {
 
   // create spark session if not provided by environment
   implicit val session: SparkSession = globalConfig.createSparkSession("test", Some("local[*]"))
-  implicit val context = Helpers.getDefaultExecContext(registry)
+  import session.implicits._
+  implicit val context = TestUtil.getDefaultActionPipelineContext(registry)
 
   // get data object of the registry
   val dataObject = registry.get[CsvFileDataObject]("ab-csv-org")
